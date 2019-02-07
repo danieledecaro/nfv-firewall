f08711792a06c16b933b82569032fcf79a26b817
raerith <danieledecaro113@gmail.com>
2019-02-07 22:54:58 +0100

    added array notation for multiple rules

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 echo $deny
 IFS=';' read -ra ADDR <<< "$deny"
-for i in "$ADDR"; do
+for i in "${ADDR[@]}"; do
     # $i has rule in form src,dest
-    IFS=', ' read -ra SRCDST <<< "$i"
+    IFS=',' read -ra SRCDST <<< "$i"
     #####IFS=':' read -ra SRCPORT <<< ${SRCDST[0]}
     #####IFS=':' read -ra DSTPORT <<< ${SRCDST[1]}
     echo "iptables -A FORWARD -s ${SRCDST[0]} -d ${SRCDST[1]} -j REJECT"
@@ -13,9 +13,9 @@ done
 
 echo $allow
 IFS=';' read -ra ADDR <<< "$allow"
-for i in "$ADDR"; do
+for i in "${ADDR[@]}"; do
     # $i has rule in form src,dest
-    IFS=', ' read -ra SRCDST <<< "$i"
+    IFS=',' read -ra SRCDST <<< "$i"
     #####IFS=':' read -ra SRCPORT <<< ${SRCDST[0]}
     #####IFS=':' read -ra DSTPORT <<< ${SRCDST[1]}
     echo "iptables -A FORWARD -s ${SRCDST[0]} -d ${SRCDST[1]} -j ACCEPT"
@@ -23,8 +23,10 @@ for i in "$ADDR"; do
 done
 
 if [[ $defaultAction = [Aa][Ll][Ll][Oo][Ww] ]]; then
+    echo "default policy accept"
     iptables -t filter -A FORWARD -j ACCEPT
 elif [[ $defaultAction = [Dd][Ee][Nn][Yy] ]]; then
+    echo "default policy reject"
     iptables -t filter -A FORWARD -j REJECT
 fi
 
